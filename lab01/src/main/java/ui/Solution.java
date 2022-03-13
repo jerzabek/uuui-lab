f89c061502042b246f19b535a4bdf679cc0c84c4
Ivan <jerzabek.ivan@gmail.com>
2022-03-13 14:54:05 +0100

    Lab01 Initial commit

---
+++ lab01/src/main/java/ui/Solution.java
@@ -0,0 +1,13 @@
+package ui;
+
+public class Solution {
+
+  public static void main(String... args) {
+    System.out.println("Ovime kreće Vaš program.");
+		
+    for (String arg : args) {
+      System.out.printf("Predan argument %s%n", arg);
+    }
+  }
+
+}
